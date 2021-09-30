b4e8bbcd9cf4bbeda0feaad6cd90912da18bec25
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2021-09-30 08:29:49 -0300

    Melhorando Christmas_lights adicionando botões

--- Christmas_lights/script.js
+++ Christmas_lights/script.js
@@ -1,5 +1,17 @@
 var frase = "Bom dia!!";
-setInterval(trocar, 1000);
+var fa = setInterval(trocar, 1000);
+var kl =  setInterval(brilhar, 1000)
+clearInterval(kl)
+clearInterval(fa)
+
+function Festa(){
+    clearInterval(kl)
+    fa = setInterval(trocar, 1000);
+}
+function Intensidade(){
+    clearInterval(fa)
+    var kl =  setInterval(brilhar, 1000)
+}
 
 function trocar(){
     for(let i=1;i<17;i++){
@@ -7,6 +19,21 @@ function trocar(){
         let b = sortear();
         let c = sortear();
         document.getElementById(i).style.backgroundColor = 'rgb('+a+','+b+','+c+')';
+    }
+}
+function intensidade(){
+    let k = Math.random()
+    return k;
+}
+function brilhar(){
+    let a = sortear();
+    let b = sortear();
+    let c = sortear();
+    for(let i=1;i<17;i++){
+            
+        let k = intensidade()
+        console.log(k)
+        document.getElementById(i).style.backgroundColor = 'rgb('+a+','+b+','+c+','+k+')';
     }
 }
 
